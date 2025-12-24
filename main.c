5a5ed0a3a08064abfe1ce5181a86099f58947b02
arnaudvalette <contact@valettearnaud.dev>
2025-12-24 14:01:04 +0100

    switch to ncurses_get_blocking, avoid busy loop

--- main.c
+++ main.c
@@ -40,25 +40,24 @@ void *handle_input(void *_arg) {
     }
     pthread_mutex_unlock(&poll_mtx);
 
-    if (notcurses_get_nblock(ctx->nc, &ni) > 0) {
+    notcurses_get_blocking(ctx->nc, &ni);
 
-      /* TODO: this is a hack as for now notcurses consider
-         mouse moving (y<0 || x<0) to be an ESC press on kitty and ghostty
-       */
-      pthread_mutex_lock(&poll_mtx);
-      if ((ni.id == NCKEY_ESC && ni.evtype == NCTYPE_RELEASE) || stop_exec) {
-        stop_exec = 1;
-        pthread_cond_broadcast(&poll_cv);
-        pthread_mutex_unlock(&poll_mtx);
-        break;
-      }
-
-      if (ui_changed(&ni)) {
-        ui_dirty = 1;
-        pthread_cond_broadcast(&poll_cv);
-      }
+    /* TODO: this is a hack as for now notcurses consider
+       mouse moving (y<0 || x<0) to be an ESC press on kitty and ghostty
+     */
+    pthread_mutex_lock(&poll_mtx);
+    if ((ni.id == NCKEY_ESC && ni.evtype == NCTYPE_RELEASE) || stop_exec) {
+      stop_exec = 1;
+      pthread_cond_broadcast(&poll_cv);
       pthread_mutex_unlock(&poll_mtx);
+      break;
     }
+
+    if (ui_changed(&ni)) {
+      ui_dirty = 1;
+      pthread_cond_broadcast(&poll_cv);
+    }
+    pthread_mutex_unlock(&poll_mtx);
 
     usleep(10);
   }
