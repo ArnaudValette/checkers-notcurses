0a47ba02563ae60f42346824f4e555ced441c644
arnaudvalette <contact@valettearnaud.dev>
2026-01-11 19:31:26 +0100

    dynamic rules: may describe the game fully

--- logic.c
+++ logic.c
@@ -19,18 +19,18 @@ bool initRules(){
   if(rules==NULL) return false;
 
   /* Basic rules for pawns */
-  Rule pawn_kill_front_right = {.kill_location=V2(1,1),.end_location=V2(2,2), .target_cell=TCell_empty, .type=Rule_killing_move_hookable};
-  Rule pawn_kill_front_left = {.kill_location=V2(1,-1),.end_location=V2(2,-2), .target_cell=TCell_empty, .type=Rule_killing_move_hookable};
+  Rule pawn_kill_front_right = {.kill_location=V2(1,1),.end_location=V2(2,2), .target_cell=TCell_empty, .type=Rule_killing_move_hookable, .next_type=Next_killing, .direction=MD_FR, .bind=Pawn_bound};
+  Rule pawn_kill_front_left = {.kill_location=V2(1,-1),.end_location=V2(2,-2), .target_cell=TCell_empty, .type=Rule_killing_move_hookable, .next_type=Next_killing, .direction=MD_FL, .bind=Pawn_bound};
   rules[n_rules++] = pawn_kill_front_right; 
   rules[n_rules++] = pawn_kill_front_left;
 
-  Rule pawn_kill_back_right = {.kill_location=V2(-1,1),.end_location=V2(-2,2), .target_cell=TCell_empty, .type=Rule_killing_move_hookable};
-  Rule pawn_kill_back_left = {.kill_location=V2(-1,-1),.end_location=V2(-2,-2), .target_cell=TCell_empty, .type=Rule_killing_move_hookable};
+  Rule pawn_kill_back_right = {.kill_location=V2(-1,1),.end_location=V2(-2,2), .target_cell=TCell_empty, .type=Rule_killing_move_hookable, .next_type=Next_killing, .direction=MD_BR, .bind=Pawn_bound};
+  Rule pawn_kill_back_left = {.kill_location=V2(-1,-1),.end_location=V2(-2,-2), .target_cell=TCell_empty, .type=Rule_killing_move_hookable, .next_type=Next_killing, .direction=MD_BL, .bind=Pawn_bound};
   rules[n_rules++] = pawn_kill_back_left;
   rules[n_rules++] = pawn_kill_back_right;
 
-  Rule pawn_move_front_right = {.kill_location=V2(0,0),.end_location=V2(1,1), .target_cell=TCell_empty, .type=Rule_pacific_move};
-  Rule pawn_move_front_left = {.kill_location=V2(0,0),.end_location=V2(1,-1), .target_cell=TCell_empty, .type=Rule_pacific_move};
+  Rule pawn_move_front_right = {.kill_location=V2(0,0),.end_location=V2(1,1), .target_cell=TCell_empty, .type=Rule_pacific_move, .next_type=Next_null, .direction=MD_FR, .bind=Pawn_bound};
+  Rule pawn_move_front_left = {.kill_location=V2(0,0),.end_location=V2(1,-1), .target_cell=TCell_empty, .type=Rule_pacific_move, .next_type=Next_null, .direction=MD_FL, .bind=Pawn_bound};
   rules[n_rules++] = pawn_move_front_left;
   rules[n_rules++] = pawn_move_front_right;
 
--- logic.h
+++ logic.h
@@ -6,17 +6,20 @@
 #define kingMove(__col, __row)                                                 \
   (KingHelper) { (__col) + (__row) * 10, getCell((__col), (__row)) }
 
+typedef enum {MD_F, MD_FR, MD_R, MD_BR, MD_B, MD_BL, MD_L, MD_FL} Move_direction;
 typedef enum {TCell_empty, TCell_occupied, TCell_killed} Target_cell_state;
 typedef enum {Rule_killing_move_hookable, Rule_pacific_move} Rule_type;
-/* no king for now */
-//typedef enum {King_bound, Pawn_bound} Bind_type;
+typedef enum {Next_pacific, Next_null, Next_any, Next_any_same_direction, Next_killing} Next_type;
+typedef enum {King_bound, Pawn_bound} Bind_type;
 
 typedef struct rule{
   V2 end_location;
   V2 kill_location;
   Target_cell_state target_cell;
+  Move_direction direction;
   Rule_type type;
-  //Bind_type bind;
+  Next_type next_type;
+  Bind_type bind;
 } Rule;
 
 typedef struct move {
