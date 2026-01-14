aef5302b33d4965c5af43da3d7f5c65c60a9b768
arnaudvalette <contact@valettearnaud.dev>
2026-01-14 14:02:36 +0100

    dyn_arr -> deque

--- datastruct.h
+++ datastruct.h
@@ -4,24 +4,142 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#define DEQUE_INITIAL_SIZE 128
+
+#ifndef DATASTRUCT_USE_VOID_BASED_DEQUE
+
 /*
 ╰┭━╾┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅╼━┮╮
-╭╯ datastructures > dynamic arrays                                          ╭╯╿
+╭╯ datastructures § array deque → macro implementation                      ╭╯╿
 ╙╼━╾┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄━━╪*/
 
-#define DYN_ARR_INITIAL_SIZE 128
+#define DEQUE_TYPEDECL(type, name) \
+  typedef struct{\
+    type **data; \
+    size_t start; \
+    size_t end; \
+    size_t capacity; \
+} name##_deque;
 
-typedef struct dyn_arr {
+#define DEQUE_IMPL(type, name) \
+static name##_deque * name##_deque_create() { \
+  name##_deque *a = malloc(sizeof(name##_deque)); \
+  if (!a) return NULL; \
+  a->capacity = DEQUE_INITIAL_SIZE; \
+  a->data = malloc(a->capacity * sizeof(type *)); \
+  if (!a->data) { \
+    free(a); \
+    return NULL; \
+  } \
+  a->start = a->end = 0; \
+  return a; \
+} \
+static inline int name##_deque_is_empty(name##_deque *a) { return a->end == a->start; }\
+ \
+static size_t name##_deque_len(name##_deque *a) { \
+  return (a->end + a->capacity - a->start) % a->capacity; \
+} \
+ \
+static type * name##_deque_get(name##_deque *a, size_t i) { \
+  if (i >= name##_deque_len(a)) { \
+    return NULL; \
+  } \
+  return a->data[(a->start + i) % a->capacity]; \
+} \
+ \
+static void name##_deque_destroy(name##_deque *a) { \
+  free(a->data); \
+  a->data = NULL; \
+  free(a); \
+  a = NULL; \
+} \
+ \
+static inline int name##__deque_full(name##_deque *a) { \
+  return (a->end + 1) % a->capacity == a->start; \
+} \
+ \
+static inline int name##__deque_resize(name##_deque *a) { \
+  if (a->capacity > (SIZE_MAX) / 2 / sizeof(type *)) { \
+    return 1; \
+  } \
+  size_t len = name##_deque_len(a); \
+  size_t new_cap = a->capacity * 2; \
+ \
+  type **data = malloc(new_cap * sizeof(type *)); \
+  if (!data) return 1; \
+ \
+  for (size_t i = 0; i < len; i++) { \
+    data[i] = a->data[(a->start + i) % a->capacity]; \
+  } \
+  free(a->data); \
+  a->data = data; \
+  a->capacity = new_cap; \
+  a->start = 0; \
+  a->end = len; \
+  return 0; \
+} \
+ \
+/** @brief append to the end of the array */ \
+static int name##_deque_append(name##_deque *a, type *data) { \
+  if (name##__deque_full(a)) { \
+    if (name##__deque_resize(a)) { \
+      return 1; \
+    } \
+  } \
+  a->data[a->end] = data; \
+  a->end = (a->end + 1) % a->capacity; \
+  return 0; \
+} \
+ \
+/** @return the first element of the array (removed). */ \
+static type * name##_deque_pop(name##_deque *a) { \
+  if (name##_deque_is_empty(a)) { \
+    return NULL; \
+  } \
+  type *v = a->data[a->start]; \
+  a->start = (a->start + 1) % a->capacity; \
+  return v; \
+} \
+ \
+/** @return the last element of the array (removed). */ \
+static type * name##_deque_pop_last(name##_deque *a) { \
+  if (name##_deque_is_empty(a)) { \
+    return NULL; \
+  } \
+  a->end = (a->end + a->capacity - 1) % a->capacity; \
+  return a->data[a->end]; \
+} \
+ \
+/** @brief push to the start of the array */ \
+static int name##_deque_push(name##_deque *a, type *data) { \
+  if (name##__deque_full(a)) { \
+    if (name##__deque_resize(a)) { \
+      return 1; \
+    } \
+  } \
+  a->start = (a->start + a->capacity - 1) % a->capacity; \
+  a->data[a->start] = data; \
+  return 0; \
+}
+
+#else
+
+/*
+╰┭━╾┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅╼━┮╮
+╭╯ datastructures § array deque → (void *) implementation                   ╭╯╿
+╙╼━╾┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄━━╪*/
+
+typedef struct deque {
   void **data;
-  unsigned int start;
-  unsigned int end;
+  size_t start;
+  size_t end;
   size_t capacity;
-} dyn_arr;
+} deque;
 
-static dyn_arr *dyn_arr_create() {
-  dyn_arr *a = malloc(sizeof(dyn_arr));
+static deque *deque_create() {
+  deque *a = malloc(sizeof(deque));
   if (!a) return NULL;
-  a->capacity = DYN_ARR_INITIAL_SIZE;
+  a->capacity = DEQUE_INITIAL_SIZE;
   a->data = malloc(a->capacity * sizeof(void *));
   if (!a->data) {
     free(a);
@@ -31,35 +149,35 @@ static dyn_arr *dyn_arr_create() {
   return a;
 }
 
-static inline int dyn_arr_is_empty(dyn_arr *a) { return a->end == a->start; }
+static inline int deque_is_empty(deque *a) { return a->end == a->start; }
 
-static size_t dyn_arr_len(dyn_arr *a) {
+static size_t deque_len(deque *a) {
   return (a->end + a->capacity - a->start) % a->capacity;
 }
 
-static void *dyn_arr_get(dyn_arr *a, size_t i) {
-  if (i >= dyn_arr_len(a)) {
+static void *deque_get(deque *a, size_t i) {
+  if (i >= deque_len(a)) {
     return NULL;
   }
   return a->data[(a->start + i) % a->capacity];
 }
 
-static void dyn_arr_destroy(dyn_arr *a) {
+static void deque_destroy(deque *a) {
   free(a->data);
   a->data = NULL;
   free(a);
   a = NULL;
 }
 
-static inline int __dyn_arr_full(dyn_arr *a) {
+static inline int __deque_full(deque *a) {
   return (a->end + 1) % a->capacity == a->start;
 }
 
-static inline int __dyn_arr_resize(dyn_arr *a) {
+static inline int __deque_resize(deque *a) {
   if (a->capacity > (SIZE_MAX) / 2 / sizeof(void *)) {
     return 1;
   }
-  size_t len = dyn_arr_len(a);
+  size_t len = deque_len(a);
   size_t new_cap = a->capacity * 2;
 
   void **data = malloc(new_cap * sizeof(void *));
@@ -77,9 +195,9 @@ static inline int __dyn_arr_resize(dyn_arr *a) {
 }
 
 /** @brief append to the end of the array */
-static int dyn_arr_append(dyn_arr *a, void *data) {
-  if (__dyn_arr_full(a)) {
-    if (__dyn_arr_resize(a)) {
+static int deque_append(deque *a, void *data) {
+  if (__deque_full(a)) {
+    if (__deque_resize(a)) {
       return 1;
     }
   }
@@ -89,8 +207,8 @@ static int dyn_arr_append(dyn_arr *a, void *data) {
 }
 
 /** @return the first element of the array (removed). */
-static void *dyn_arr_pop(dyn_arr *a) {
-  if (dyn_arr_is_empty(a)) {
+static void *deque_pop(deque *a) {
+  if (deque_is_empty(a)) {
     return NULL;
   }
   void *v = a->data[a->start];
@@ -99,8 +217,8 @@ static void *dyn_arr_pop(dyn_arr *a) {
 }
 
 /** @return the last element of the array (removed). */
-static void *dyn_arr_pop_last(dyn_arr *a) {
-  if (dyn_arr_is_empty(a)) {
+static void *deque_pop_last(deque *a) {
+  if (deque_is_empty(a)) {
     return NULL;
   }
   a->end = (a->end + a->capacity - 1) % a->capacity;
@@ -108,9 +226,9 @@ static void *dyn_arr_pop_last(dyn_arr *a) {
 }
 
 /** @brief push to the start of the array */
-static int dyn_arr_push(dyn_arr *a, void *data) {
-  if (__dyn_arr_full(a)) {
-    if (__dyn_arr_resize(a)) {
+static int deque_push(deque *a, void *data) {
+  if (__deque_full(a)) {
+    if (__deque_resize(a)) {
       return 1;
     }
   }
@@ -119,9 +237,11 @@ static int dyn_arr_push(dyn_arr *a, void *data) {
   return 0;
 }
 
+#endif
+
 /*
 ╰┭━╾┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅╼━┮╮
-╭╯ datastructures > hashmap                                                 ╭╯╿
+╭╯ datastructures § hashmap → macro based implementation                    ╭╯╿
 ╙╼━╾┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄━━╪*/
 
 #define HASHMAP(kType, vType, name)\
@@ -136,7 +256,11 @@ typedef struct{ \
 
 /*
 ╰┭━╾┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅╼━┮╮
-╭╯ datastructures > branching system                                        ╭╯╿
+╭╯ datastructures § branching system → implementation                       ╭╯╿
+╙╼━╾┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄━━╪*/
+/*
+╰┭━╾┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅╼━┮╮
+╭╯ datastructures § branching system → documentation                        ╭╯╿
 ╙╼━╾┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄━━╪*/
 
 #endif
